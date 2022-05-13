f9660ad090e7a167d82c762948ba13e107972615
ramin709 <79803446+ramin709@users.noreply.github.com>
2022-05-13 11:57:04 +0430

    complete BestSellers
    HotBestSellers component completed

--- src/components/HotBestSellers/HotBestSellers.tsx
+++ src/components/HotBestSellers/HotBestSellers.tsx
@@ -1,17 +1,117 @@
 import React from 'react'
-import Carousel from '../Carousel/Carousel'
 import './HotBestSellers.css'
+import Rating from '@mui/material/Rating'
+import Image from '../../images/phone.png'
 
 const HotBestSellers = () => {
   return (
     <section className="hotBestSection">
-        <div className="hotBestContainer">
-            <div className="hotBestTitle">
-                <h3>Hot Best Sellers</h3>
-                <hr />
-            </div>
-            
+      <div className="hotBestContainer">
+        <div className="hotBestTitle">
+          <h3>Hot Best Sellers</h3>
+          <hr />
         </div>
+
+        <div className="hotBestItemsContainer">
+          <div className="hotBestItem">
+            <div className="hotBestItemImg">
+              <img src={Image} alt="" />
+            </div>
+            <div className="hotBestItemInfo">
+              <span className="category">Headphone</span>
+              <span className="productName">Xiaomi Redmi Note 4</span>
+              <div className="rating">
+                <Rating name="read-only" value={4} readOnly />
+              </div>
+              <div className="prices">
+                <span className="oldPrice">$300</span>
+                <span className="newPrice">$255</span>
+              </div>
+            </div>
+          </div>
+          <div className="hotBestItem">
+            <div className="hotBestItemImg">
+              <img src={Image} alt="" />
+            </div>
+            <div className="hotBestItemInfo">
+              <span className="category">Headphone</span>
+              <span className="productName">Xiaomi Redmi Note 4</span>
+              <div className="rating">
+                <Rating name="read-only" value={4} readOnly />
+              </div>
+              <div className="prices">
+                <span className="oldPrice">$300</span>
+                <span className="newPrice">$255</span>
+              </div>
+            </div>
+          </div>
+          <div className="hotBestItem">
+            <div className="hotBestItemImg">
+              <img src={Image} alt="" />
+            </div>
+            <div className="hotBestItemInfo">
+              <span className="category">Headphone</span>
+              <span className="productName">Xiaomi Redmi Note 4</span>
+              <div className="rating">
+                <Rating name="read-only" value={4} readOnly />
+              </div>
+              <div className="prices">
+                <span className="oldPrice">$300</span>
+                <span className="newPrice">$255</span>
+              </div>
+            </div>
+          </div>
+          <div className="hotBestItem">
+            <div className="hotBestItemImg">
+              <img src={Image} alt="" />
+            </div>
+            <div className="hotBestItemInfo">
+              <span className="category">Headphone</span>
+              <span className="productName">Xiaomi Redmi Note 4</span>
+              <div className="rating">
+                <Rating name="read-only" value={4} readOnly />
+              </div>
+              <div className="prices">
+                <span className="oldPrice">$300</span>
+                <span className="newPrice">$255</span>
+              </div>
+            </div>
+          </div>
+          <div className="hotBestItem">
+            <div className="hotBestItemImg">
+              <img src={Image} alt="" />
+            </div>
+            <div className="hotBestItemInfo">
+              <span className="category">Headphone</span>
+              <span className="productName">Xiaomi Redmi Note 4</span>
+              <div className="rating">
+                <Rating name="read-only" value={4} readOnly />
+              </div>
+              <div className="prices">
+                <span className="oldPrice">$300</span>
+                <span className="newPrice">$255</span>
+              </div>
+            </div>
+          </div>
+          <div className="hotBestItem">
+            <div className="hotBestItemImg">
+              <img src={Image} alt="" />
+            </div>
+            <div className="hotBestItemInfo">
+              <span className="category">Headphone</span>
+              <span className="productName">Xiaomi Redmi Note 4</span>
+              <div className="rating">
+                <Rating name="read-only" value={4} readOnly />
+              </div>
+              <div className="prices">
+                <span className="oldPrice">$300</span>
+                <span className="newPrice">$255</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </section>
   )
 }
